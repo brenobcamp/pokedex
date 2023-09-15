5e8932b49a7317eedd674a7972607db5628a480d
Breno Batista Campos <brenobatistacampos@gmail.com>
2023-09-15 15:32:57 +0000

    começando com o js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,18 @@
 // btnTeste.addEventListener("click", function (){
 //     console.log("Clicou no botão")
 // });
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then(function (response){
+        console.log(response)
+        })
+    .catch(function(error) {
+        console.error(error)
+    })
+    .finally( function (){
+        console.log("Requisição concluída!");
+    });
