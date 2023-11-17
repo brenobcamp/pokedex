1274fabbfcc9d8724b66d8f8154083deb17aa585
Breno Batista Campos <brenobatistacampos@gmail.com>
2023-11-17 12:04:26 +0000

    configurando model

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,31 @@
-// const btnTeste = document.getElementById("btnTeste");
-// btnTeste.addEventListener("click", function (){
-//     console.log("Clicou no botão")
-// });
-
 const offset = 0;
 const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 const listPokemons = document.querySelector('#pokemonList')
 
 function convertPokemonToHtml (pokemon){
     return `
     <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-        </li>
-    `
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+
+            <img src="${pokemon.photo}"
+                 alt="${pokemon.name}">
+        </div>
+    </li>
+`
 }
 
-fetch(url)
-    .then(response => response.json())
-    .then(jsonBody => jsonBody.results)
-    .then(pokemonList => {
-        const pokemonsArr = pokemonList.map((pokemon) => {
-            return convertPokemonToHtml(pokemon)
-        })
 
-        const newHtml = pokemonsArr.join("")
-
-        listPokemons.innerHTML = newHtml
-
+function loadPokemonItens(offset, limit) {
+    PokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToHtml).join('')
+        listPokemons.innerHTML += newHtml
     })
-    .catch(function(error) {
-        console.error(error)
-    })
-    .finally( function (){
-        console.log("Requisição concluída!");
-    });
+}
 
-
-    // <ol class="types">
-                    {/* ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')} */}
-                {/* </ol> */}
+loadPokemonItens(offset, limit)
