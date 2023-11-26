672b7411b1cb0e46b70aec2151b0ef4cdb50e67e
Breno Batista Campos <brenobatistacampos@gmail.com>
2023-11-26 21:54:13 +0000

    funcionalidades interativas da página  funcionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,39 +3,52 @@ const limit = 20;
 const maxRecords = 151;
 const listPokemons = document.querySelector('#pokemonList')
 const loadMoreBtn = document.querySelector('#loadMoreButton');
+// Criando janela popup
+const janelaPopup = document.createElement('div');
+const divFecharJanelaPopup = document.createElement('div');
+const divNomeImagemPokemon = document.createElement('div');
+const divDetalhesPokemon = document.createElement('div');
+const botaoFechar = document.createElement('button');
+const divFundoFalso = document.createElement('div');
+divFundoFalso.classList.add('fundo-falso');
+botaoFechar.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>';
+botaoFechar.onclick = function () { 
+    document.body.removeChild(janelaPopup) 
+    document.body.removeChild(divFundoFalso)
+}
 
 async function mostrarPopUp (pokemonNumber) {
-    // const fundoFalso = document.createElement('div');
-    const popup = document.createElement('div');
-    const abaPopUp = document.createElement('div');
-    const botaoFechar = document.createElement('button');
+    document.body.appendChild(divFundoFalso)
     const pokemonDetail = await PokeApi.getPokemonById(pokemonNumber)
+    janelaPopup.className = `popup ${pokemonDetail.type}`;
+
+    divFecharJanelaPopup.appendChild(botaoFechar)
+    janelaPopup.appendChild(divFecharJanelaPopup)
     
-    // fundoFalso.classList.add('fundo-falso')
-    // document.body.appendChild(fundoFalso)
-
-    botaoFechar.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>';
-    botaoFechar.onclick = function () { document.body.removeChild(popup) }
-    popup.appendChild(botaoFechar);
-
-    popup.className = `popup ${pokemonDetail.type}`;
-    popup.innerHTML += `
+    divNomeImagemPokemon.innerHTML = `
     <h1>${pokemonDetail.name}</h1><br>
     <img src="${pokemonDetail.photo}">
     `
-    abaPopUp.className = `conteudo-popup ${pokemonDetail.type}`
-    abaPopUp.innerHTML = `
+    janelaPopup.appendChild(divNomeImagemPokemon)
+
+    divDetalhesPokemon.className = `conteudo-popup ${pokemonDetail.type}`
+    divDetalhesPokemon.innerHTML = `
         <h2>About</h2>
-        <h3>Species: ${pokemonDetail.type}</h3>
+        <h3>Type: ${pokemonDetail.type}</h3>
         <h3>Weight: ${pokemonDetail.weight}</h3>
         <h3>Height: ${pokemonDetail.height}</h3>
     `
-
-    popup.appendChild(abaPopUp);
-    document.body.appendChild(popup)
-
-    
+    janelaPopup.appendChild(divDetalhesPokemon);
+    document.body.appendChild(janelaPopup)
 }
+
+window.addEventListener('click', (event) => {
+    if (event.target.classList == 'fundo-falso'){
+        document.body.removeChild(janelaPopup) 
+        document.body.removeChild(divFundoFalso)
+    }
+})
+
 
 function convertPokemonToHtml (pokemon){
     return `
